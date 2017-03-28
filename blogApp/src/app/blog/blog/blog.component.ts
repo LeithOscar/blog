f7f07eb4cec283e20c329aca35ffd9d37b78fd6e
Jarayseh Gutierrez <leith.jarayseh@external.wolterskluwer.com>
2017-03-28 15:45:41 +0200

    header

--- blogApp/src/app/blog/blog/blog.component.ts
+++ blogApp/src/app/blog/blog/blog.component.ts
@@ -1,15 +1,35 @@
+//imports use other modules
 import { Component, OnInit } from '@angular/core';
 
+//metadata, describe the component
 @Component({
   selector: 'app-blog',
   templateUrl: './blog.component.html',
   styleUrls: ['./blog.component.css']
 })
-export class BlogComponent implements OnInit {
 
+//class define our component
+export class BlogComponent  implements OnInit {
+
+  recentPost = [
+    {
+      postId:'1',
+      title:'First post',
+      autor:'leith',
+      date :'01/03/2017',
+      topics:[
+        'angular 2',
+        'typeScript'
+      ]
+    }
+  ];
+  
   constructor() { }
 
   ngOnInit() {
   }
 
 }
+
+
+/*all views must be a comopnent associated*/
