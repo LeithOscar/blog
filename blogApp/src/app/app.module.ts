f721eae976e30e44eb2f3ffbcd7d843a68573d98
Leith Jarayseh Gutierrez <Leith@iMac-de-Leith.local>
2017-04-01 11:30:35 +0200

    logon component

--- blogApp/src/app/app.module.ts
+++ blogApp/src/app/app.module.ts
@@ -6,16 +6,19 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { BlogComponent } from './blog/blog/blog.component';
 import { Routes,RouterModule  } from '@angular/router';
+import { LoginComponent } from './login/login.component';
 
 const appRoutes: Routes = [
-  { path: 'blog',   component: BlogComponent }
+  { path: 'blog',   component: BlogComponent },
+  { path: 'login',   component: LoginComponent }
 ];
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    BlogComponent
+    BlogComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
